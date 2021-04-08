48f31f55c848fd7f3b94c4e5b53c6e46a6fc1352
Octaniel <otanieljose30@gmail.com>
2021-04-08 11:09:08 +0000

    make it better

--- src/main/java/com/bsoftware/lpbp/config/ResourceServerConfig.java
+++ src/main/java/com/bsoftware/lpbp/config/ResourceServerConfig.java
@@ -51,7 +51,7 @@ class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.cors().and().authorizeRequests().antMatchers("/pessoa","/usuario", "/oauth/token", "/presenca").permitAll()
+        http.cors().and().authorizeRequests().antMatchers("/pessoa","/usuario/listar", "/oauth/token", "/presenca").permitAll()
                 .anyRequest()
                 .authenticated().and()
                 .sessionManagement()
